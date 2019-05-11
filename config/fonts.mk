995d87825fd6444e0a2bde5cfcf7cf6b0a226063
spezi77 <spezi7713@gmx.net>
2019-05-11 19:50:37 +0200

    vendor: fonts: Add fonts for Lockscreen
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,4 +1,4 @@
-# Fonts
+# Google Fonts
 PRODUCT_COPY_FILES += \
     vendor/aosip/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
     vendor/aosip/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
@@ -6,5 +6,23 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
     vendor/aosip/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
     vendor/aosip/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+# Extended system fonts
+PRODUCT_COPY_FILES += \
+    vendor/aosip/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/aosip/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/aosip/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/aosip/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/aosip/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/aosip/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
+    vendor/aosip/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
+    vendor/aosip/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
+    vendor/aosip/fonts/mexcellent/mexcellent.ttf:system/fonts/Mexcellent-Regular.ttf \
+    vendor/aosip/fonts/burnstown/burnstown.ttf:system/fonts/Burnstown-Regular.ttf \
+    vendor/aosip/fonts/dumbledor/dumbledor.ttf:system/fonts/Dumbledor-Regular.ttf \
+    vendor/aosip/fonts/PhantomBold/PhantomBold.ttf:system/fonts/PhantomBold-Regular.ttf \
+    vendor/aosip/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
+    vendor/aosip/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
+    vendor/aosip/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
 
 ADDITIONAL_FONTS_FILE := vendor/aosip/fonts/google-sans.xml
