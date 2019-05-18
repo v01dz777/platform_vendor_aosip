db2233e7e5d5583135d21771d2fc16063d085641
NurKeinNeid <mralexman3000@gmail.com>
2019-05-18 23:21:20 +0200

    vendor: Don't ship Updater
     * It doesn't make sense in my case.

--- config/packages.mk
+++ config/packages.mk
@@ -10,8 +10,7 @@ PRODUCT_PACKAGES += \
     messaging \
     Recorder \
     WeatherClient \
-    WellbeingPrebuilt \
-    Updater
+    WellbeingPrebuilt
 
 # DerpFest packages
 PRODUCT_PACKAGES += \
