da2e8f245a8be91e7edffadbb4d5a080a46d07dc
ElDainosor <eldainosor@gmail.com>
2019-08-17 19:59:14 +0200

    vendor: Import La fontage v2.2 from Bootleggers [3/3]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -29,6 +29,50 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/fonts/PhantomBold/PhantomBold.ttf:system/fonts/PhantomBold-Regular.ttf \
     vendor/aosip/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/aosip/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
-    vendor/aosip/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
+    vendor/aosip/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf \
+    vendor/aosip/fonts/fontage/AdamCGPro-Regular.ttf:system/fonts/AdamCGPro-Regular.ttf \
+    vendor/aosip/fonts/fontage/AlexanaNeue-Regular.ttf:system/fonts/AlexanaNeue-Regular.ttf \
+    vendor/aosip/fonts/fontage/AlienLeague-Regular.ttf:system/fonts/AlienLeague-Regular.ttf \
+    vendor/aosip/fonts/fontage/Azedo-Light.ttf:system/fonts/Azedo-Light.ttf \
+    vendor/aosip/fonts/fontage/BigNoodleTilting-Italic.ttf:system/fonts/BigNoodleTilting-Italic.ttf \
+    vendor/aosip/fonts/fontage/BigNoodleTilting-Regular.ttf:system/fonts/BigNoodleTilting-Regular.ttf \
+    vendor/aosip/fonts/fontage/Biko-Regular.ttf:system/fonts/Biko-Regular.ttf \
+    vendor/aosip/fonts/fontage/Blern-Regular.ttf:system/fonts/Blern-Regular.ttf \
+    vendor/aosip/fonts/fontage/CoCoBiker-Regular.ttf:system/fonts/CoCoBiker-Regular.ttf \
+    vendor/aosip/fonts/fontage/Fester-Medium.ttf:system/fonts/Fester-Medium.ttf \
+    vendor/aosip/fonts/fontage/GinoraSans-Regular.ttf:system/fonts/GinoraSans-Regular.ttf \
+    vendor/aosip/fonts/fontage/Inkferno-Regular.ttf:system/fonts/Inkferno-Regular.ttf \
+    vendor/aosip/fonts/fontage/Instruction-Regular.ttf:system/fonts/Instruction-Regular.ttf \
+    vendor/aosip/fonts/fontage/JackLane-Regular.ttf:system/fonts/JackLane-Regular.ttf \
+    vendor/aosip/fonts/fontage/Metropolis1920-Regular.ttf:system/fonts/Metropolis1920-Regular.ttf \
+    vendor/aosip/fonts/fontage/Monad-Regular.ttf:system/fonts/Monad-Regular.ttf \
+    vendor/aosip/fonts/fontage/Neoneon-Regular.ttf:system/fonts/Neoneon-Regular.ttf \
+    vendor/aosip/fonts/fontage/Noir-Regular.ttf:system/fonts/Noir-Regular.ttf \
+    vendor/aosip/fonts/fontage/North-Regular.ttf:system/fonts/North-Regular.ttf \
+    vendor/aosip/fonts/fontage/OutrunFuture-Regular.ttf:system/fonts/OutrunFuture-Regular.ttf \
+    vendor/aosip/fonts/fontage/Qontra-Regular.ttf:system/fonts/Qontra-Regular.ttf \
+    vendor/aosip/fonts/fontage/Riviera-Regular.ttf:system/fonts/Riviera-Regular.ttf \
+    vendor/aosip/fonts/fontage/TheOutbox-Regular.ttf:system/fonts/TheOutbox-Regular.ttf \
+    vendor/aosip/fonts/fontage/Union-Regular.ttf:system/fonts/Union-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Abel-Regular.ttf:system/fonts/Abel-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/AdventPro-Regular.ttf:system/fonts/AdventPro-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/ArchivoNarrow-Regular.ttf:system/fonts/ArchivoNarrow-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/AutourOne-Regular.ttf:system/fonts/AutourOne-Regular.ttf \
+	vendor/aosip/fonts/fontagev2/Bariol-Regular.ttf:system/fonts/Bariol-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/BadScript-Regular.ttf:system/fonts/BadScript-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/CherrySwash-Regular.ttf:system/fonts/CherrySwash-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Codystar.ttf:system/fonts/Codystar.ttf \
+    vendor/aosip/fonts/fontagev2/IBMPlexMono.ttf:system/fonts/IBMPlexMono.ttf \
+    vendor/aosip/fonts/fontagev2/IBMPlexMono-Light.ttf:system/fonts/IBMPlexMono-Light.ttf \
+    vendor/aosip/fonts/fontagev2/Jura-Regular.ttf:system/fonts/Jura-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/KellySlab-Regular.ttf:system/fonts/KellySlab-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Pompiere-Regular.ttf:system/fonts/Pompiere-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Raleway-Light.ttf:system/fonts/Raleway-Light.ttf \
+    vendor/aosip/fonts/fontagev2/ReemKufi-Regular.ttf:system/fonts/ReemKufi-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Satisfy-Regular.ttf:system/fonts/Satisfy-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/SeaweedScript-Regular.ttf:system/fonts/SeaweedScript-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/SedgwickAveDisplay-Regular.ttf:system/fonts/SedgwickAveDisplay-Regular.ttf \
+    vendor/aosip/fonts/fontagev2/Vibur.ttf:system/fonts/Vibur.ttf \
+    vendor/aosip/fonts/fontagev2/Voltaire.ttf:system/fonts/Voltaire.ttf
 
 ADDITIONAL_FONTS_FILE := vendor/aosip/fonts/google-sans.xml
